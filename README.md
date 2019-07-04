de8fa05994813c3448ef4f3a61f9a58fa85c87ed
Isaias Arellano <iarellano@nearbpo.com>
2019-07-04 08:50:27 -0500

    Clean up of plugin, soapction header included in log if present

--- README.md
+++ README.md
@@ -31,11 +31,12 @@ sla-reporting:
     name: "sla-reporting"
     stream:
       path: "/var/tmp/sla-reporting.log"
-      period: '1d'
-      totalFiles: 20
-      rotateExisting: false
-      threshold: '10m'
-      gzip: false
+      period: '1d'           # daily rotation
+      totalFiles: 20         # keep up to 10 back copies
+      rotateExisting: false  # Give ourselves a clean file when we start up, based on period
+      threshold: '10m'       # Rotate log files larger than 10 megabytes
+      #totalSize: '20m'       # Don't keep more than 20mb of archived log files
+      gzip: false            # Don't compress the archive log files to save space
 
 ```
 
--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 var SplunkLogger = require("splunk-logging").Logger;
 var Bunyan = require("bunyan");
 var RotatingFileStream = require('bunyan-rotating-file-stream');
+var debug = require('debug')('plugin:sla-reporting');
 
 
 module.exports.init = function(config, logger, stats) {
 
     var splunk;
     var bunyan;
-    if (config.splunk && config.splunk.host) {
+    if (config.splunk && config.splunk.config) {
         splunk = new SplunkLogger(config.splunk.config);
     }
     if (config.bunyan && config.bunyan.stream) {
@@ -38,22 +39,37 @@ module.exports.init = function(config, logger, stats) {
                 forwarded_ip: req.headers['x-forwarded-for'],
                 useragent: req.headers['user-agent'],
                 apiproxy_revision: res.proxy.revision,
-
-                client_received_start_timestamp: now,
-                client_received_end_timestamp: now + 1
+                client_received_start_timestamp: Date.now()
             };
+
+            if (req.headers['soapaction']) {
+                record.soapaction = req.headers['soapaction'];
+            }
 
             req.sla_reporting = record;
             next();
         },
 
+        onend_request: function(req, res, data, next) {
+            var record = req.sla_reporting;
+            var now = Date.now();
+            record.client_received_end_timestamp = now;
+            record.target_sent_start_timestamp = now + 1;
+            record.target_sent_end_timestamp = now + 2;
+            next(null, data);
+        },
+
+        onresponse: function(req, res, data, next) {
+            var record = req.sla_reporting;
+            record.target_received_start_timestamp = Date.now();
+            next(null, data);
+        },
+
         onend_response: function(req, res, data, next) {
 
             var record = req.sla_reporting;
-            var now = Date.now();
             record.response_status_code      = res.statusCode;
-            record.client_sent_start_timestamp = now;
-            record.client_sent_end_timestamp = now+1;
+
 
             var token = req.token;
             if (token) {
@@ -65,7 +81,6 @@ module.exports.init = function(config, logger, stats) {
                 var prodList = token.api_product_list;
                 if (prodList && prodList.length) {
                     if (typeof prodList === 'string') { prodList = prodList.slice(1, -1).split(','); }
-                    // hack: analytics server only accepts a single product
                     record.api_product = prodList[0];
                 }
                 record.bcbsa_client_id = token.bcbsa_client_id;
@@ -73,11 +88,10 @@ module.exports.init = function(config, logger, stats) {
                 record.client_cn = token.client_cn;
 
             }
-
-            record.target_sent_start_timestamp = req.headers['target_sent_start_timestamp'];
-            record.target_sent_end_timestamp = req.headers['target_sent_start_timestamp'] + 1;
-            record.target_received_start_timestamp = req.headers['target_received_start_timestamp'];
             record.target_received_end_timestamp = req.headers['target_received_start_timestamp'] + 1;
+            record.client_sent_start_timestamp = Date.now();
+            next(null, data);
+            record.client_sent_end_timestamp = Date.now();
 
             var payload = {
                 metadata: config.splunk.metadata,
@@ -85,15 +99,13 @@ module.exports.init = function(config, logger, stats) {
             };
 
             if (bunyan) {
-                bunyan.info("Helo there");
                 bunyan.info(record, "sla-reporting");
             }
             if (splunk) {
                 splunk.send(payload, function (err, resp, body) {
-                    console.log("Response from Splunk", body);
+                    debug("Response from Splunk", body);
                 });
             }
-            next(null, data);
         }
     };
 };
